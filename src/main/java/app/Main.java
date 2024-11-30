d7839270e9b3de6bec0a4ee74e8ea201ad7163e9
Alex <olexiyskok@gmail.com>
2024-11-30 19:15:20 +0200

    add count users in list

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         UserRepository userRepository = new UserRepository();
-        userRepository.findUserById(1);
+        userRepository.findUserById(12);
         userRepository.findUserByEmail("petro@gmail.com");
         userRepository.findAllUsers();
     }
--- src/main/java/app/user/UserRepository.java
+++ src/main/java/app/user/UserRepository.java
@@ -13,24 +13,27 @@ public class UserRepository {
     User user4 = new User(4, "Vasyl", "vasyl@gmail.com");
 
     List<User> userList = Arrays.asList(user1, user2, user3, user4);
+    List<User> userList2 = Arrays.asList();
 //    by id
    public void findUserById(int id){
        Optional<Integer> optionalId = Optional.of(id);
        Stream<User> userStream = userList.stream()
                .filter(user -> user.getId() == optionalId.get());
-       System.out.println("Optional за ID: " + id + " " + userStream.findAny());
+       System.out.println("Optional за ID: " + id + " - " + userStream.findAny());
    }
 //    by email
     public void findUserByEmail(String email){
         Optional<String> optionalEmail = Optional.of(email);
         Stream<User> userStream = userList.stream()
                 .filter(user -> user.getEmail().equals(optionalEmail.get()));
-        System.out.println("Optional за email: " + email + " " + userStream.findAny());
+        System.out.println("Optional за email: " + email + " - " + userStream.findAny());
     }
 //    all users
     public void findAllUsers(){
-       Optional<List<User>> listOptional = Optional.ofNullable(userList);
+        Optional<List<User>> listOptional = Optional.ofNullable(userList);
+        int countUser = listOptional.get().size();
+        System.out.println("Весь список/n");
         System.out.println("All users: " + listOptional);
+        System.out.println("Кількість юзерів у списку " + countUser);
     }
-
 }
